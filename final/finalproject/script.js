60723b7c96629332820b1fc71710c06abcc46d79
hannah_lei <hlanderson@ucdavis.edu>
2023-03-22 16:25:05 -0700

    new

--- final/finalproject/script.js
+++ final/finalproject/script.js
@@ -5,9 +5,7 @@
     const container = document.querySelector('#container');
     const hotSpots = document.querySelectorAll('#container div');
     const theImg = document.querySelector('div img');
-    // will be used to determine which hotspot div you mouse over, if any.
     let thisSpot;
-    // will be used to determine when the mouse has stopped moving.
     let movingMouse;
 
     const raspButton = document.querySelector('#raspbutton a');
@@ -23,43 +21,36 @@
 
 
     // Zoom in and out with hover
-
     function zoomPhoto(event){
-        const thisCorner = event.target.id;
-        // console.log(`zooming into ${thisSpot}`);
-        switch (thisCorner){
+        const thisSpot = event.target.id;
+        switch (thisSpot){
             case 'raspberry': theImg.className = 'raspberry';
             setTimeout(function(){
                 raspButton.className = "showing";
-                // console.log("Raspberry");
             }, 500)
             break;
 
             case 'salmon': theImg.className = 'salmon';
             setTimeout(function(){
                 salButton.className = "showing";
-                // console.log("Salmon");
             }, 500);
             break;
 
             case 'pineapple': theImg.className = 'pineapple'; 
             setTimeout(function(){
                 pineButton.className = "showing";
-                // console.log("Pineapple");
             }, 500);
             break;
 
             case 'dimsum': theImg.className = 'dimsum'; 
             setTimeout(function(){
                 dimButton.className = "showing";
-                // console.log("Dimsum");
             }, 500);
             break;
             
             case 'ramen': theImg.className = 'ramen';
             setTimeout(function(){
                 ramButton.className = "showing";
-                // console.log("Ramen");
             }, 500);
             break;
         }
@@ -68,9 +59,6 @@
 
     hotSpots.forEach(function (eachSpot) {
         eachSpot.addEventListener('mouseover', zoomPhoto);
-        eachSpot.addEventListener('mouseover', function(){
-            document.querySelector('body').className = 'pagecolor2';
-        });
 
         eachSpot.addEventListener('mouseout', function () {
             theImg.className = 'start';
@@ -85,40 +73,7 @@
             });
         });
     });
-    // Add event listeners to each of the hotspots
-    // hotSpots.forEach(function (eachSpot) {
-    //     eachSpot.addEventListener('mouseover', zoomPhoto);
-    //     eachSpot.addEventListener('mouseover', function(){
-    //         document.querySelector('body').className = 'pagecolor2';
-    //     });
 
-    //     eachSpot.addEventListener('mouseout', function () {
-    //         thisSpot = 'out';
-    //         console.log(thisSpot);
-
-    //         document.querySelector('body').className = 'pagecolor1';            
-    //         hideButton();
-
-    //         document.addEventListener('keydown', function(e){
-    //             if(e.key === 'Escape'){
-    //                 theImg.className = 'start';
-    //             }
-    //         });
-
-
-    //     });
-    // });
-
-    /* This event listener fires while the mouse is moving.
-    If the value of thisSpot is 'out' you are not over a hotspot. 
-    If you stop moving the mouse for a second or longer while not
-    over a hotspot, the image zooms out.
-    
-    This seems to keep the dizzying effects of constantly zooming in and
-    out as you mouse in and out of the hotspots to a minimum, and stops the 
-    unintended effect of having the image sometimes slide out of the container
-    entirely.
-    */
     document.addEventListener('mousemove', function(){
         clearTimeout(movingMouse);
         if(thisSpot == 'out'){
@@ -159,17 +114,16 @@
 
     // Hide button when zooming out
     function hideButton(){
-        // fruitButton.className = "hidden"
         for(let i = 0; i < fruitButton.length; i++){
-            //console.log(fruitButton[i])
             fruitButton[i].className = "hidden";
         }
     }
 
-    document.querySelector('#music').addEventListener('click', function(e){
+    // Spin lazy susan
+    document.querySelector('#spin').addEventListener('click', function(e){
         e.preventDefault();
-        document.querySelector('#music').innerHTML += '<audio controls autoplay><source src="beats.wav" type="audio/wav" class="hidden"></audio>';
-        // document.querySelector('#music').innerHTML += 'Stop Music';
+        document.querySelector('#lazysusan').className = 'spin';
+        document.querySelector('#spin').innerHTML += '<audio controls autoplay><source src="beats.wav" type="audio/wav" class="hidden"></audio>';
     });
 
 
